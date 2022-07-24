66d3de5cdb7f4c9bd03ae3c82f0e1edf868f1da6
Isadora Vasconcellos <ivasconcellos@iff.edu.br>
2022-07-23 21:12:43 -0300

    Configured EJS and Bootstrap.

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
 const express = require("express");
 const app = express();
 
+const path = require('path')
+
 const { urlencoded } = require("express");
 
+//Use Json format
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+
+//configuration EJS
+app.set("view engine", "ejs");
+
+//Configuratio Static Files
+app.use(express.static("public"));
+app.use(
+    express.static(path.join(__dirname, "node_modules/bootstrap/dist/"))
+);
  
+//Main route
 app.get('/', function (req, res) {
-    res.send('Hello World')
+    return res.render("index");
 });
 
+//Run server
 app.listen(8080, () => {
    console.log("Servidor rodando!");
 });
-
 
 module.exports = app;
