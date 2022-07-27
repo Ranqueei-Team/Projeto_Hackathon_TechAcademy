a3ba445c5ddcac2dc3f3a8086bae2831f8d7b84c
Isadora Vasconcellos <ivasconcellos@iff.edu.br>
2022-07-27 20:44:48 -0300

    Configurated index.js

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ const expressLayouts = require('express-ejs-layouts');
 const passport = require('passport');
 const flash = require('connect-flash');
 const session = require('express-session');
-
-const app = express();
+const path = require('path')
 require('./config/passport')(passport);
 
-const path = require('path')
+//Express
+const app = express();
+
+//Env file configuration
 require('dotenv').config();
 const { urlencoded } = require("express");
 
@@ -15,15 +17,12 @@ const { urlencoded } = require("express");
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-//configuration EJS
+//EJS configuration
 app.set("view engine", "ejs");
 
-//Configuration Static Files
+//Static Files Configuration
 app.use(express.static("public"));
-app.use(
-    express.static(path.join(__dirname, "node_modules/bootstrap/dist/"))
-);
- 
+app.use(express.static(path.join(__dirname, "node_modules/bootstrap/dist/")));
 app.use('/tinymce', express.static(path.join(__dirname, 'node_modules', 'tinymce')));
 app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist/'));
  
@@ -35,51 +34,9 @@ const Reward = require("./models/rewardModel");
 const Team = require("./models/teamModel");
 const Profile = require("./models/profileModel");
 
-//Database connection
-const connection = require("./database/database");
-
-//Start database connection
-connection.authenticate()
-    .then( () => {
-        console.log("Conexão estabelecida com sucesso!");
-    }).catch( (error) => {
-        console.log("Erro ao conectar o banco de dados:", error);
-});
-
-//Main route
+//Main route configuration
 app.get('/', function (req, res) {
     return res.render("index");
-});
-
-//Session 
-app.use(
-    session({
-    secret: 'secret',
-    resave: true,
-    saveUninitialized: true
-   })
-);
-
-//Passport middleware
-app.use(passport.initialize());
-app.use(passport.session());
-require('./config/passport')(passport);
-
-//Current User
-app.use(function(req,res,next){
-    res.locals.currentUser = req.user;
-    next();
-  })
-
-//Flash middleware
-app.use(flash());
-
-// Global variables middleware
-app.use(function(req, res, next) {
-    res.locals.success_msg = req.flash('success_msg');
-    res.locals.error_msg = req.flash('error_msg');
-    res.locals.error = req.flash('error');
-    next();
 });
 
 //Routes
@@ -95,6 +52,48 @@ const rewardsRoute = require("./routes/rewardsRoute");
 app.use("/rewards/", rewardsRoute);
 const teamsRoute = require("./routes/teamsRoute");
 app.use("/teams/", teamsRoute);
+
+//Database connection
+const connection = require("./database/database");
+
+//Start database connection
+connection.authenticate()
+    .then( () => {
+        console.log("Conexão estabelecida com sucesso!");
+    }).catch( (error) => {
+        console.log("Erro ao conectar o banco de dados:", error);
+});
+
+//Session configuration
+app.use(
+    session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+   })
+);
+
+//Passport middleware configuration
+app.use(passport.initialize());
+app.use(passport.session());
+require('./config/passport')(passport);
+
+//Current User
+app.use(function(req,res,next){
+    res.locals.currentUser = req.user;
+    next();
+  })
+
+//Flash middleware
+app.use(flash());
+
+//Global variables middleware
+app.use(function(req, res, next) {
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    res.locals.error = req.flash('error');
+    next();
+});
 
 //Run server
 const PORT = process.env.PORT;
