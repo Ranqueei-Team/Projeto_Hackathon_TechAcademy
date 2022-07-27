ce96433714d94a8bcdeb0dfec57d1a338c38fc32
Isadora Vasconcellos <ivasconcellos@iff.edu.br>
2022-07-27 16:09:35 -0300

    Config better-errors.

--- index.js
+++ index.js
@@ -3,7 +3,12 @@ const expressLayouts = require('express-ejs-layouts');
 const passport = require('passport');
 const flash = require('connect-flash');
 const session = require('express-session');
-
+const { BetterError } = require('better-errors');
+try{
+    functionCall()
+}catch(error){
+    console.log(BetterError(error));
+}
 const app = express();
 require('./config/passport')(passport);
 
