b7579d008f511c49e8884916f312f2a5dc2548eb
Isadora Vasconcellos <ivasconcellos@iff.edu.br>
2022-07-28 00:56:26 -0300

    Configured syncdatabase.

--- database/syncDatabase.js
+++ database/syncDatabase.js
@@ -2,8 +2,8 @@ const Sequelize = require("sequelize");
 const sequelize = require("../database/database")
 
 //Create tables
-//sequelize.sync({ force: false })
+sequelize.sync({ force: false })
 
 //Force recreate tables
-sequelize.sync({ force: true })
+//sequelize.sync({ force: true })
 
